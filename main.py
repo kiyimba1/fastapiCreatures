677f772794d0188ab65b9cff1bff2e69f09345f4
Eddie <edwinkiyimba@gmail.com>
2024-08-12 15:01:33 +0300

    Added the creature web layer

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from fastapi import FastAPI
-from web import explorer
+from web import explorer, creature
 
 app = FastAPI()
 
-@app.get("/")
-def top():
-    return "top here"
-
-@app.get("/echo/{thing}")
-def echo(thing):
-    return f"echoing {thing}"
 
 app.include_router(explorer.router)
+app.include_router(creature.router)
 
 if __name__ == "__main__":
     import uvicorn
